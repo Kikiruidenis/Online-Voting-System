859a90a2d541ac5d1718aa39147594743096d18c
Kikiruidenis <39088264+Kikiruidenis@users.noreply.github.com>
2022-03-31 12:08:07 +0300

    Add files via upload
    First Commit-Login and registration pages

---
+++ Online-Voting-System/routes/dashboard.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Online Voting System -Dashboard</title>
+    </head>
+    <body>
+        
+    <button>Back</button>
+    <button>Logout</button>
+    <h1>Online Voting System</h1>
+    
+    <div id="Profile"></div>
+    <div id="group"></div>
+    </body>
+</html>
